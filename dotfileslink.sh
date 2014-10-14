7f98787f74e610e213a7f3cd94f8476829ddcf8d
komekome09 <fanmaskome@gmail.com>
2014-10-14 20:49:49 +0900

    add .ssh/config

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.ch_proxy.sh ~/.ch_proxy.sh
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.ssh/config ~/.ssh/config
