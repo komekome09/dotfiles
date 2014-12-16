585500f9f295b29653bb634a84f3525cfd99c550
komekome09 <fanmaskome@gmail.com>
2014-12-16 13:55:24 +0900

    delete line "ln -s $HOME/dotfiles/.vim ~/.vim"

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -7,7 +7,6 @@ ln -s $HOME/dotfiles/.gitconfig ~/.gitconfig
 ln -s $HOME/dotfiles/.ch_proxy.sh ~/.ch_proxy.sh
 ln -s $HOME/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s $HOME/dotfiles/.ssh/config ~/.ssh/config
-ln -s $HOME/dotfiles/.vim ~/.vim
 ln -s $HOME/dotfiles/.gemrc ~/.gemrc
 ln -s $HOME/dotfiles/.tmux ~/.tmux
 ln -s $HOME/dotfiles/.gitignore ~/.gitignore
