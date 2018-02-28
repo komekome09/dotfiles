8521595a9852a96fa5581226d44e5c59ce097e4c
komekome09 <fanmaskome@gmail.com>
2018-03-01 02:52:01 +0900

    neobundle -> dein

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -2,7 +2,7 @@
 
 ln -s $HOME/dotfiles/.zshrc ~/.zshrc
 ln -s $HOME/dotfiles/.vimrc ~/.vimrc
-ln -s $HOME/dotfiles/.vimrc.neobundle ~/.vimrc.neobundle
+ln -s $HOME/dotfiles/.vimrc.dein ~/.vimrc.dein
 ln -s $HOME/dotfiles/.gitconfig ~/.gitconfig
 ln -s $HOME/dotfiles/.ch_proxy.sh ~/.ch_proxy.sh
 ln -s $HOME/dotfiles/.tmux.conf ~/.tmux.conf
